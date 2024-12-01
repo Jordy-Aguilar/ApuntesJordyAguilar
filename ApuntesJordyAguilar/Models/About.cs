9b355dd36ac89a803e9c3e7d87b1984732b8e806
Jordy Aguilar <jeay022@gmail.com>
2024-11-30 21:42:53 -0500

    Cree modelos About, AllNotes y Note

---
+++ ApuntesJordyAguilar/Models/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApuntesJordyAguilar.Models
+{
+    public class About
+    {
+        public string Title { get; set; } = "Notes Jordy Aguilar";
+        public string Version { get; set; } = AppInfo.VersionString;
+        public string Message { get; set; } = "Hola, soy Jordy Aguilar, soy de Zaruma, tengo 20 años, me gusta el futbol y el anime :)";
+        public string MoreInfoUrl { get; set; } = "https://aka.ms/maui";
+    }
+}
---
+++ ApuntesJordyAguilar/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApuntesJordyAguilar.Models
+{
+    class Note
+    {
+        public string Filename { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
+
