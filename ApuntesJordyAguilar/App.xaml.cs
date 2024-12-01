09121bb64173b08df1fe41dda85cb9ab7e532f98
Jordy Aguilar <jeay022@gmail.com>
2024-11-30 21:01:00 -0500

    Agregar archivos de proyecto.

---
+++ ApuntesJordyAguilar/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ApuntesJordyAguilar
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
