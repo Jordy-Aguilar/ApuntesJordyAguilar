a2eeb48ccdedb499a4d1c71990b61c24a70f26e8
Jordy Aguilar <jeay022@gmail.com>
2024-11-30 22:38:42 -0500

    Agregue navegaciones Tabs y Flyout, para las dos paginas, about y Notes

--- ApuntesJordyAguilar/Models/About.cs
+++ ApuntesJordyAguilar/Models/About.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ApuntesJordyAguilar.Models
+﻿namespace ApuntesJordyAguilar.Models
 {
     public class About
     {
-        public string Title { get; set; } = "Notes Jordy Aguilar";
-        public string Version { get; set; } = AppInfo.VersionString;
-        public string Message { get; set; } = "Hola, soy Jordy Aguilar, soy de Zaruma, tengo 20 años, me gusta el futbol y el anime :)";
-        public string MoreInfoUrl { get; set; } = "https://aka.ms/maui";
+       public string Title => "Notes Jordy Aguilar";
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "Hola, soy Jordy Aguilar, soy de Zaruma, tengo 20 años, me gusta el futbol y el anime :)";
+        public string Image => "meme.jpg";
+        public string Version => AppInfo.VersionString;
     }
 }
